8eee8facc809ac14bfcadabca3ad2c3416062462
Oleksandr Brui <oleksandr.bruigit@gmail.com>
2021-02-28 22:21:53 +0200

    Create index.html and js_calculateCost.js

---
+++ js_calculateCost.js
@@ -0,0 +1,18 @@
+'use strict';
+
+calculateCost(
+  {
+    display: 20,
+    wheel: 100,
+    cpu: 40,
+  },
+  {
+    amazobot: {
+      wheel: 12.5,
+    },
+    robozetka: {
+      display: 56.2,
+      cpu: 150,
+    },
+  }
+);
